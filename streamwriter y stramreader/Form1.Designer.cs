099e908b25df884305cc030f0397bb7c81fd55a2
juan <juan@DESKTOP-UMCEALU>
2023-02-25 23:39:02 -0600

    Agregar archivos de proyecto.

---
+++ streamwriter y stramreader/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace streamwriter_y_stramreader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnArchivo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(87, 78);
+            this.txtArchivo.Multiline = true;
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtArchivo.Size = new System.Drawing.Size(549, 347);
+            this.txtArchivo.TabIndex = 0;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnGuardar.Location = new System.Drawing.Point(188, 28);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(106, 34);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnBorrar.Location = new System.Drawing.Point(336, 28);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(106, 34);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnArchivo
+            // 
+            this.btnArchivo.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnArchivo.Location = new System.Drawing.Point(494, 28);
+            this.btnArchivo.Name = "btnArchivo";
+            this.btnArchivo.Size = new System.Drawing.Size(106, 34);
+            this.btnArchivo.TabIndex = 3;
+            this.btnArchivo.Text = "Abrir Archvo";
+            this.btnArchivo.UseVisualStyleBackColor = false;
+            this.btnArchivo.Click += new System.EventHandler(this.btnArchivo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnArchivo);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtArchivo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnArchivo;
+    }
+}
+
---
+++ streamwriter y stramreader/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace streamwriter_y_stramreader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtArchivo.Clear();
+            txtArchivo.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog paquito;
+            paquito=new SaveFileDialog();
+            paquito.Filter = "Nombre del Archivo|+.txt";
+           if (paquito.ShowDialog() == DialogResult.OK) 
+            {
+                StreamWriter escribir;
+                escribir=new StreamWriter(paquito.FileName);
+                escribir.Write(txtArchivo.Text); escribir.Close();
+            }
+        }
+
+        private void btnArchivo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog lector;
+            lector = new OpenFileDialog();
+            lector.Filter = "Nombre del Archivo|+.txt";
+            if (lector.ShowDialog()==DialogResult.OK) 
+            {
+                StreamReader abrir;
+                abrir = new StreamReader(lector.FileName);
+               txtArchivo.Text = abrir.ReadToEnd(); abrir.Close();
+            }
+        }
+    }
+}
